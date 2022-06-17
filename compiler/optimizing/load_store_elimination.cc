3ac41e32ce2e725192067e62af4a1aa3a58489b4
Santiago Aboy Solanes <solanes@google.com>
2022-06-17 12:04:17 +0100

    Get the ClassLoader from the DexCache in ReferenceTypePropagation
    We were passing it down as a parameter when we can simply get it
    from the dex cache. Passing it down as a parameter brings the
    possibility of using the wrong class loader and can be the source
    of bugs.
    
    Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
    Change-Id: I7fbf3d6f79186d4eb695c77b876291d02e3170ba

--- compiler/optimizing/load_store_elimination.cc
+++ compiler/optimizing/load_store_elimination.cc
@@ -1829,7 +1829,6 @@ static HInstruction* FindOrConstructNonLoopPhi(
   if (type == DataType::Type::kReference) {
     // Update reference type information. Pass invalid handles, these are not used for Phis.
     ReferenceTypePropagation rtp_fixup(block->GetGraph(),
-                                       Handle<mirror::ClassLoader>(),
                                        Handle<mirror::DexCache>(),
                                        /* is_first_run= */ false);
     rtp_fixup.Visit(phi);
@@ -2416,7 +2415,6 @@ bool LSEVisitor::MaterializeLoopPhis(ArrayRef<const size_t> phi_placeholder_inde
     }
     // Update reference type information. Pass invalid handles, these are not used for Phis.
     ReferenceTypePropagation rtp_fixup(GetGraph(),
-                                       Handle<mirror::ClassLoader>(),
                                        Handle<mirror::DexCache>(),
                                        /* is_first_run= */ false);
     rtp_fixup.Visit(ArrayRef<HInstruction* const>(phis));
@@ -3075,7 +3073,6 @@ class PartialLoadStoreEliminationHelper {
       return;
     }
     ReferenceTypePropagation rtp_fixup(GetGraph(),
-                                       Handle<mirror::ClassLoader>(),
                                        Handle<mirror::DexCache>(),
                                        /* is_first_run= */ false);
     rtp_fixup.Visit(ArrayRef<HInstruction* const>(new_ref_phis_));
--- compiler/optimizing/reference_type_propagation.h
+++ compiler/optimizing/reference_type_propagation.h
@@ -32,7 +32,6 @@ namespace art HIDDEN {
 class ReferenceTypePropagation : public HOptimization {
  public:
   ReferenceTypePropagation(HGraph* graph,
-                           Handle<mirror::ClassLoader> class_loader,
                            Handle<mirror::DexCache> hint_dex_cache,
                            bool is_first_run,
                            const char* name = kReferenceTypePropagationPassName);
@@ -73,8 +72,6 @@ class ReferenceTypePropagation : public HOptimization {
       REQUIRES_SHARED(Locks::mutator_lock_);
 
   void ValidateTypes();
-
-  Handle<mirror::ClassLoader> class_loader_;
 
   // Note: hint_dex_cache_ is usually, but not necessarily, the dex cache associated with
   // graph_->GetDexFile(). Since we may look up also in other dex files, it's used only
