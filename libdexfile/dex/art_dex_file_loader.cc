hopez13/art

Make ArtDexFileLoader accept an zip file with no dex code.
We pass config split APKs as part of the class loader context to
OatFileAssistant, in both the legacy dexopt code path and the new ART
Services code path. Those config split APKs eventually go to
ArtDexFileLoader. Therefore, ArtDexFileLoader should not regard this as
an error.

Before this change, this was regarded as an error. The legacy dexopt
code path could work because it uses the legacy
`DexFile.GetDexOptNeeded` Java API, which doesn't check the result of
opening dex files. The new ART Services code path couldn't work because
it checks the result.

Bug: 229268202
Test: m test-art-host-gtest-art_libdexfile_tests
Test: (on internal master) -
  1. Run `pm art optimize-package` for a split APK that depends on a
     config split APK.
  2. See dexopt being successful.
Change-Id: I08a2a5ad7bea82c3f534ed4de2935fc400652361

libdexfile/dex/art_dex_file_loader.cc
@@ -114,18 +114,22 @@ bool ArtDexFileLoader::GetMultiDexChecksums(const char* filename,
       return false;
     }
 
+    if (zip_file_only_contains_uncompressed_dex != nullptr) {
+      // Start by assuming everything is uncompressed.
+      *zip_file_only_contains_uncompressed_dex = true;
+    }
+
     uint32_t idx = 0;
     std::string zip_entry_name = GetMultiDexClassesDexName(idx);
     std::unique_ptr<ZipEntry> zip_entry(zip_archive->Find(zip_entry_name.c_str(), error_msg));
     if (zip_entry.get() == nullptr) {
-      *error_msg = StringPrintf("Zip archive '%s' doesn't contain %s (error msg: %s)", filename,
-          zip_entry_name.c_str(), error_msg->c_str());
-      return false;
-    }
-
-    if (zip_file_only_contains_uncompressed_dex != nullptr) {
-      // Start by assuming everything is uncompressed.
-      *zip_file_only_contains_uncompressed_dex = true;
+      // An zip file with no dex code should be accepted. It's likely a config split APK, which we
+      // are currently passing from higher levels.
+      LOG(INFO) << StringPrintf("Zip archive '%s' doesn't contain %s (error msg: %s)",
+                                filename,
+                                zip_entry_name.c_str(),
+                                error_msg->c_str());
+      return true;
     }
 
     do {

bananadroid/android_art

Test: m test-art-host-gtest
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

libdexfile/dex/art_dex_file_loader.cc
@@ -19,9 +19,9 @@
 #include <sys/stat.h>
 
 #include "android-base/stringprintf.h"
-
 #include "base/file_magic.h"
 #include "base/file_utils.h"
+#include "base/logging.h"
 #include "base/mem_map.h"
 #include "base/mman.h"  // For the PROT_* and MAP_* constants.
 #include "base/stl_util.h"
@@ -114,18 +114,22 @@ bool ArtDexFileLoader::GetMultiDexChecksums(const char* filename,
       return false;
     }
 
+    if (zip_file_only_contains_uncompressed_dex != nullptr) {
+      // Start by assuming everything is uncompressed.
+      *zip_file_only_contains_uncompressed_dex = true;
+    }
+
     uint32_t idx = 0;
     std::string zip_entry_name = GetMultiDexClassesDexName(idx);
     std::unique_ptr<ZipEntry> zip_entry(zip_archive->Find(zip_entry_name.c_str(), error_msg));
     if (zip_entry.get() == nullptr) {
-      *error_msg = StringPrintf("Zip archive '%s' doesn't contain %s (error msg: %s)", filename,
-          zip_entry_name.c_str(), error_msg->c_str());
-      return false;
-    }
-
-    if (zip_file_only_contains_uncompressed_dex != nullptr) {
-      // Start by assuming everything is uncompressed.
-      *zip_file_only_contains_uncompressed_dex = true;
+      // A zip file with no dex code should be accepted. It's likely a config split APK, which we
+      // are currently passing from higher levels.
+      VLOG(dex) << StringPrintf("Zip archive '%s' doesn't contain %s (error msg: %s)",
+                                filename,
+                                zip_entry_name.c_str(),
+                                error_msg->c_str());
+      return true;
     }
 
     do {