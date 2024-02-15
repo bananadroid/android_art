hopez13/art

Remove empty ifs
If we had an HIf where both branches were empty, we wouldn't emit
any code generation for the HIf. However, this still emits other
instructions e.g. outer ifs, instructions used in the if guard.

This CL detects when an HIf is empty and can be eliminated during
DCE.

Bug: 305225992
Fixes: 305225992
Test: art/test/testrunner/testrunner.py --host --64 -b --optimizing
Change-Id: I81ff5ee1326b6a27480b1af88d41955f1ca2b823

test/2266-checker-remove-empty-ifs/src/Main.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+    public static void main(String[] args) {}
+    public static void $inline$empty() {}
+    public static void $inline$empty2() {}
+
+    /// CHECK-START: void Main.andCall2(boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andCall2(boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andCall2(boolean a, boolean b) {
+        if (a && b) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.andCall3(boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andCall3(boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andCall3(boolean a, boolean b, boolean c) {
+        if (a && b && c) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.andCall4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andCall4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andCall4(boolean a, boolean b, boolean c, boolean d) {
+        if (a && b && c && d) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orCall2(boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orCall2(boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orCall2(boolean a, boolean b) {
+        if (a || b) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orCall3(boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orCall3(boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orCall3(boolean a, boolean b, boolean c) {
+        if (a || b || c) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orCall4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orCall4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orCall4(boolean a, boolean b, boolean c, boolean d) {
+        if (a || b || c || d) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.intCall(int, int, int, int) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.intCall(int, int, int, int) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void intCall(int a, int b, int c, int d) {
+        if (a <= b && c <= d && a >= 20 && b <= 78 && c >= 50 && d <= 70) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    class MyObject {
+        boolean inner;
+        Object inner2;
+    }
+
+    /// CHECK-START: void Main.objectCall(Main$MyObject) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.objectCall(Main$MyObject) dead_code_elimination$after_inlining (before)
+    /// CHECK: InstanceFieldGet
+
+    /// CHECK-START: void Main.objectCall(Main$MyObject) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    /// CHECK-NOT: InstanceFieldGet
+    public static void objectCall(MyObject o) {
+        if (o != null && o.inner) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+}

bananadroid/android_art

test/2266-checker-remove-empty-ifs/src/Main.java
@@ -1,0 +1,152 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+    public static void main(String[] args) {}
+    public static void $inline$empty() {}
+    public static void $inline$empty2() {}
+
+    /// CHECK-START: void Main.andBoolean2(boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andBoolean2(boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andBoolean2(boolean a, boolean b) {
+        if (a && b) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.andBoolean3(boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andBoolean3(boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andBoolean3(boolean a, boolean b, boolean c) {
+        if (a && b && c) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.andBoolean4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andBoolean4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andBoolean4(boolean a, boolean b, boolean c, boolean d) {
+        if (a && b && c && d) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orBoolean2(boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orBoolean2(boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orBoolean2(boolean a, boolean b) {
+        if (a || b) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orBoolean3(boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orBoolean3(boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orBoolean3(boolean a, boolean b, boolean c) {
+        if (a || b || c) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.orBoolean4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.orBoolean4(boolean, boolean, boolean, boolean) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void orBoolean4(boolean a, boolean b, boolean c, boolean d) {
+        if (a || b || c || d) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    /// CHECK-START: void Main.andInt(int, int, int, int) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andInt(int, int, int, int) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    public static void andInt(int a, int b, int c, int d) {
+        if (a <= b && c <= d && a >= 20 && b <= 78 && c >= 50 && d <= 70) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+
+    class MyObject {
+        boolean inner;
+        boolean inner2;
+    }
+
+    /// CHECK-START: void Main.andObject(Main$MyObject) dead_code_elimination$after_inlining (before)
+    /// CHECK: If
+    /// CHECK: If
+
+    /// CHECK-START: void Main.andObject(Main$MyObject) dead_code_elimination$after_inlining (before)
+    /// CHECK: InstanceFieldGet
+    /// CHECK: InstanceFieldGet
+
+    /// CHECK-START: void Main.andObject(Main$MyObject) dead_code_elimination$after_inlining (after)
+    /// CHECK-NOT: If
+    /// CHECK-NOT: InstanceFieldGet
+    public static void andObject(MyObject o) {
+        if (o != null && o.inner && o.inner2) {
+            $inline$empty();
+        } else {
+            $inline$empty2();
+        }
+    }
+}